17b1bd1b6e1d8ca8fa8371e4b86ac540b04aaa7e
Sakerdot <adriansamatan@gmail.com>
2017-02-06 17:37:48 +0100

    Minimax algorithm
    Minimax implemented. Possible changes would be randomizing next move if
    there are more than one equally best positions on the board.

---
+++ minimax.cpp
@@ -0,0 +1,49 @@
+#include "minimax.h"
+
+// Returns true if currScore is greater than prevScore if it's maximizing or if
+// currScore is lower than prevScore if it's minimizing.
+bool switchMove(bool minimax, short currScore, short prevScore)
+{
+    return (minimax && currScore >= prevScore) || (!minimax && currScore <= prevScore);
+}
+
+// Returns the position of the next best move along with the score of said move.
+aiMove nextMove(const board &currentBoard, short length, bool minimax)
+{
+    aiMove result;
+    bool first = true;
+    
+    for (short i = 0; i < 3; ++i)
+    {
+        for (short j = 0; j < 3; ++j)
+        {
+            if (currentBoard.isEmpty(i, j))
+            {
+                board newBoard = currentBoard;
+                bool win = newBoard.makeMove(i, j);
+                
+                short scoreAtTile;
+                if (win)
+                {
+                    scoreAtTile = minimax ? 10 - (length + 1) : -10 + (length + 1);
+                }
+                else if (length == 8)
+                {
+                    scoreAtTile = 0;
+                }
+                else
+                {
+                    scoreAtTile = nextMove(newBoard, length + 1, !minimax)[0];
+                }
+                
+                if (first || switchMove(minimax, scoreAtTile, result[0]))
+                {
+                    result = { scoreAtTile, i, j };
+                    first = false;
+                }
+            }
+        }
+    }
+    
+    return result;
+}
---
+++ minimax.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <array>
+#include <vector>
+#include <random>
+#include "board.h"
+
+// @0 score, @1 row, @2 column
+typedef std::array<short, 3> aiMove;
+
+// Returns true if currScore is greater than prevScore if it's maximizing or if
+// currScore is lower than prevScore if it's minimizing.
+bool switchMove(bool minimax, short currScore, short prevScore);
+
+// Returns the position of the next best move along with the score of said move.
+aiMove nextMove(const board &currentBoard, short length, bool minimax);
